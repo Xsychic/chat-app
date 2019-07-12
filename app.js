e2a472d88196034fe1eae1a71cc4f2e0bda53040
EC2 Default User <ec2-user@ip-172-31-88-23.ec2.internal>
2019-07-12 11:22:33 +0000

    fix own index updates

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ io.on('connection', function(socket) {
                data.author = data.sender.username;
                
                 // emit new message to chat
-                socket.in(data.room).emit("im", data); 
+                io.sockets.in(data.room).emit("im", data); 
                 
                 // update users index page
                 data.users.forEach(function(user) {
@@ -147,7 +147,7 @@ io.on('connection', function(socket) {
                 chat.save();
 
                 // change title and send message to room
-                socket.in(data.room).emit("title", {room: data.room, username: data.username, title: data.title, message: message.message, users: data.users});
+                io.sockets.in(data.room).emit("title", {room: data.room, username: data.username, title: data.title, message: message.message, users: data.users});
                 
                 // create array with all users for index page
                 var indexUsers = data.users;
