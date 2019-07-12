794a28a6dbf2b0fe35ddd714446cbb52f2978b9e
EC2 Default User <ec2-user@ip-172-31-88-23.ec2.internal>
2019-07-12 11:34:07 +0000

    fix own index updates

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ io.on('connection', function(socket) {
                 
                 // update users index page
                 data.users.forEach(function(user) {
-                    io.sockets.(String(user._id)).emit("updateIm", {message: data.message, author: data.sender.username, room: data.room});
+                    io.sockets.in(String(user._id)).emit("updateIm", {message: data.message, author: data.sender.username, room: data.room});
                 });
             });
         });
